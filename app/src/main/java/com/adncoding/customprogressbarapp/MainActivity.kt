f8172b8965597450bbb2a2c4f43f6812f92a86f2
Aiden Chang <asdf62210@gmail.com>
2024-03-03 01:39:18 +0800

    Complete
    - Click on an empty space to collapse the keyboard

--- app/src/main/java/com/adncoding/customprogressbarapp/MainActivity.kt
+++ app/src/main/java/com/adncoding/customprogressbarapp/MainActivity.kt
@@ -3,6 +3,22 @@ package com.adncoding.customprogressbarapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import com.adncoding.customprogressbarapp.ui.theme.CustomProgressBarAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +26,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CustomProgressBarAppTheme {
-
+                val focusManager = LocalFocusManager.current
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onTap = { focusManager.clearFocus() }
+                            )
+                        },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    var value by remember { mutableIntStateOf(0) }
+                    CustomComponent(
+                        indicatorValue = value
+                    )
+                    OutlinedTextField(
+                        value = value.toString(),
+                        onValueChange = {
+                            value = if (it.isNotEmpty()) {
+                                it.toInt()
+                            } else 0
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onDone = {
+                                focusManager.clearFocus()
+                            }
+                        )
+                    )
+                }
             }
         }
     }
